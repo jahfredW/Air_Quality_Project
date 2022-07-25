fddd11e3000836b998db0ec53b14886b9b4b945a
jahfredW <fred.gruwe@gmail.com>
2022-07-25 14:34:59 +0200

    maj pollution data. inscription des datas en bdd via pollution_data.ajout_pollution_ville

--- data/meteo_pyowm.py
+++ data/meteo_pyowm.py
@@ -1,7 +1,10 @@
+import datetime
+
 import pyowm
 import requests
 from pyowm.utils.config import get_default_config
 from pyowm.utils import measurables
+import pollution_data
 
 class PollutionPyown:
     #------------------------
@@ -66,12 +69,46 @@ class PollutionPyown:
             pol = pollution.airpollution_manager()
             print(emplacement.lon, emplacement.lat)
             forecast = pol.air_quality_at_coords(emplacement.lat, emplacement.lon)
+            self.save_pollution_ville(ville, forecast)
         else:
             raise Exception("Attention, l'API Pollution n'a pas été initialisée")
 
         return forecast.air_quality_data
 
+    def save_pollution_ville(self, ville, forecast):
+        """
+        enregistrement des prévisions de la ville en base de données
+        :param ville: le nom de la ville
+        :param forecast: les données de pollution
+        :return:
+        """
+        if not pollution_data.ville_exists(ville):
+            raise Exception("Save meteo ville : la ville n'a pas de correspondance")
+        else:
+            id_ville = pollution_data.get_id_ville(ville)[0][0]
+            print(id_ville)
+
+        pollution_data.delete_prevision_ville(ville)
+
+        liste_poluant = ['aqi', 'co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3']
+
+        prevision_jour = {}
+        for item in liste_poluant:
+            prevision_jour[item] = forecast.air_quality_data.get(item,'no_data')
+        print(prevision_jour)
+
+        pollution_data.ajout_pollution_ville(prevision_jour['aqi'], prevision_jour['co'],prevision_jour['no'], prevision_jour['no2'], prevision_jour['o3'],
+                                             prevision_jour['so2'], prevision_jour['pm2_5'], prevision_jour['pm10'],
+                                             prevision_jour['nh3'], datetime.date.today(), id_ville)
+
+
+
+
+
+
+
+
 
 p = PollutionPyown()
-print(p.get_pollution_ville('Lille'))
+print(p.get_pollution_ville('Paris'))
 
---
+++ data/pollution_data.py
@@ -0,0 +1,166 @@
+from datetime import date
+
+import psycopg2
+
+HOST = "localhost"
+DATABASE = "prevision_db"
+USER = "dev"
+PASSWORD = "Farte512!"
+PORT = 5432
+
+def connect():
+    try:
+        connection = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
+        cursor = connection.cursor()
+        print('Version du serveur PostgreSQL:')
+        cursor.execute("SELECT * from departement")
+
+        departements = cursor.fetchall()
+        print(departements)
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if connection is not None:
+            connection.close()
+            print('connection à la BDD fermée')
+
+def get_id_ville(nom_ville: str):
+    sql = "select id_ville from ville where nom = '{0}'".format(nom_ville);
+
+    try:
+        connection = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
+        cursor = connection.cursor()
+        cursor.execute(sql)
+
+        row = cursor.fetchall()
+        connection.commit()
+        cursor.close()
+
+        return row
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if connection is not None:
+            connection.close()
+            print('connection à la BDD fermée')
+
+def ville_exists(ville: str):
+    sql = "SELECT id_ville from ville where nom='{}'".format(ville)
+
+    connection = None
+    try:
+        connection = psycopg2.connect(host=HOST, database=DATABASE, user= USER, password=PASSWORD, port=PORT)
+        cursor = connection.cursor()
+
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        print(row)
+
+        connection.commit()
+        cursor.close()
+
+        if row is not None:
+            return True
+        return False
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("error ville exits")
+        print(error)
+
+    finally:
+        if connection is not None:
+            connection.close()
+
+
+def ajout_pollution_ville(aqi,
+                          co,
+                          no,
+                          no2,
+                          o3,
+                          so2,
+                          pm2_5,
+                          pm10,
+                          nh3,
+                          last_update,
+                          id_ville):
+    """
+    Fonction qui permet d'ajouter un enregistrement dans la table prévision
+    :param id_pollution: (nom du champ se suffit à la même)
+    :param aqi: (nom du champ se suffit à la même)
+    :param co: (nom du champ se suffit à la même)
+    :param no: (nom du champ se suffit à la même)
+    :param no2: (nom du champ se suffit à la même)
+    :param o3: (nom du champ se suffit à la même)
+    :param so2: (nom du champ se suffit à la même)
+    :param pm2_5: idem
+    :param pm_10: (nom du champ se suffit à la même)
+    :param nh3: (nom du champ se suffit à la même)
+    :param last_update
+    """
+
+    sql = """ INSERT INTO pollution ( aqi, co, no, no2, o3, so2, pm2_5, pm10, nh3, last_update, id_ville) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+
+    connection = None
+    try:
+        # obtention de la connexion à la base de données
+        connection = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
+        # create a new cursor
+        cursor = connection.cursor()
+        record_to_insert = (aqi, co, no,
+                            no2, o3, so2, pm2_5, pm10,
+                            nh3, date.today(), id_ville)
+        cursor.execute(sql, record_to_insert)
+
+        # commit the changes to the database
+        connection.commit()
+        # close communication with the database
+        cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Erreur ajout_prevision_ville:")
+        print(sql)
+        print(record_to_insert)
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
+
+
+
+
+def delete_prevision_ville(nom_ville):
+    """
+    Supprime les prévisions météo pour une ville
+    :param nom_ville: nom de la ville
+    :return:
+    """
+    sql = f"DELETE FROM prevision WHERE id_ville in (select id_ville from ville where nom = '{nom_ville}');"
+
+    connection = None
+    try:
+        # obtention de la connexion à la base de données
+        connection = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
+        # create a new cursor
+        cursor = connection.cursor()
+
+        cursor.execute(sql, nom_ville)
+
+        # commit the changes to the database
+        connection.commit()
+        # close communication with the database
+        cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Erreur delete_prevision_ville:")
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
+
+get_id_ville('Paris')
